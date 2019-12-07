eaac01400626c9d375c1c855794313b715d4b6a2
lottenoorlander <lottenoorlander@gmail.com>
2019-12-07 17:27:23 +0100

    supports all restful actions

---
+++ sequelize-rest.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const bodyParserMiddleWare = bodyParser.json();
+const Sequelize = require("sequelize");
+const port = 4000;
+const databaseUrl =
+  process.env.DATABASE_URL ||
+  "postgresql://postgres:secret@localhost:5432/postgres";
+const db = new Sequelize(databaseUrl);
+
+app.use(bodyParserMiddleWare);
+app.listen(port);
+
+const Movie = db.define("movie", {
+  title: Sequelize.STRING,
+  yearOfRelease: Sequelize.INTEGER,
+  synopsis: Sequelize.STRING
+});
+
+db.sync()
+  .then(() =>
+    Movie.create({
+      title: "episode IV",
+      yearOfRelease: 1977,
+      synopsis:
+        "The Imperial Forces, under orders from cruel Darth Vader, hold Princess Leia hostage in their efforts to quell the rebellion against the Galactic Empire."
+    })
+      .then(() =>
+        Movie.create({
+          title: "episode V",
+          yearOfRelease: 1980,
+          synopsis:
+            "After the Rebels are brutally overpowered by the Empire on the ice planet Hoth, Luke Skywalker begins Jedi training with Yoda."
+        })
+      )
+      .then(() =>
+        Movie.create({
+          title: "episode VI",
+          yearOfRelease: 1983,
+          synopsis:
+            "After a daring mission to rescue Han Solo from Jabba the Hutt, the Rebels dispatch to Endor to destroy the second Death Star."
+        })
+      )
+  )
+  .catch(console.error);
+
+//post movie!
+app.post("/movies", (req, res, next) => {
+  console.log(req);
+  return Movie.create(req.body)
+    .then(movie => res.send(movie))
+    .catch(error => next(error));
+});
+
+//get all movies!
+app.get("/movies", (req, res, next) =>
+  Movie.findAll()
+    .then(results => res.send(results))
+    .catch(error => next(error))
+);
+
+// read a single movie resource!
+app.get("/movie/:id", (req, res, next) =>
+  Movie.findByPk(req.params.id).then(movieid =>
+    res.send(movieid).catch(error => next(error))
+  )
+);
+
+// update a single movie resource!
+app.put("/movie/:id", (req, res, next) =>
+  Movie.findByPk(req.params.id)
+    .then(movie => movie.update(req.body))
+    .then(movie => res.send(movie))
+    .catch(err => next(err))
+);
+
+// delete a single movie resource!
+app.delete("/movie/:id", (req, res, next) =>
+  Movie.destroy({ where: { id: req.params.id } })
+    .then(number => res.send({ number }))
+    .catch(next)
+);
